a0dbf50278e7ad8fcb66fff4ce91e2c0ac1c4ec2
webboxesdeveloper <webboxes.developer@gmail.com>
2026-01-11 19:41:23 +0100

    feat: add S3 storage support to GitHubEvent model

---
+++ db/migrate/20260111194044_add_s3_key_to_github_events.rb
@@ -0,0 +1,7 @@
+class AddS3KeyToGithubEvents < ActiveRecord::Migration[7.1]
+  def change
+    add_column :github_events, :s3_key, :string, null: true, index: true
+    # Make raw_payload nullable when S3 is used
+    change_column_null :github_events, :raw_payload, true
+  end
+end
